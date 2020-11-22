f913cd0b6f5825d875677d26940ee410f8f36935
Dominic Scanlan <dominicscanlan@hotmail.com>
2020-11-22 19:52:29 +0000

    feat: filter server items with search

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static("dist"));
 
-app.get("/api/items", (req, res) => res.send({ items }));
+app.get("/api/items", (req, res) => {
+  let menuItems = items;
+
+  try {
+    if (req.query.search.length > 0) {
+      menuItems = items.filter((item) =>
+        item.name
+          .toLocaleLowerCase()
+          .includes(req.query.search.toLocaleLowerCase())
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.send({ items: menuItems });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
