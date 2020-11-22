f61ad0f441576019b42c7d75539ba98c0619222c
Dominic Scanlan <dominicscanlan@hotmail.com>
2020-11-22 19:39:53 +0000

    feat: update Summary

--- src/client/components/summary/summary.jsx
+++ src/client/components/summary/summary.jsx
@@ -1,17 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getItems } from "../../store/selectors";
 
 const Summary = () => {
+  const items = useSelector(getItems);
+
+  const ve = items
+    ? items.filter((item) => item.dietaries.includes("ve")).length
+    : 0;
+  const v = items
+    ? items.filter((item) => item.dietaries.includes("v")).length
+    : 0;
+  const n = items
+    ? items.filter((item) => item.dietaries.includes("n!")).length
+    : 0;
+
   return (
     <div className="menu-summary">
       <div className="container">
         <div className="row">
           <div className="col-6 menu-summary-left">
-            <span>6 items</span>
+            <span>{items ? items.length : 0} items</span>
           </div>
           <div className="col-6 menu-summary-right">
-            6x <span className="dietary">ve</span>
-            4x <span className="dietary">v</span>
-            12x <span className="dietary">n!</span>
+            {ve}x <span className="dietary">ve</span>
+            {v}x <span className="dietary">v</span>
+            {n}x <span className="dietary">n!</span>
           </div>
         </div>
       </div>
